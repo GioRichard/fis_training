cbd305745d0c9fe1631d3cba38a13216da74aee8
ADMIN <giorichard604846@gmail.com>
2022-06-01 06:49:19 +0700

    init project

---
+++ Topic01_Proj1/test/java/fis/java/core/topic01/dao/mem/StudentTest.java
@@ -0,0 +1,12 @@
+package fis.java.core.topic01.dao.mem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void addStudent() {
+    }
+}
